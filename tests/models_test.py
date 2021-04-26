9dc60670588f7dbd8bb40a0fbe8c85bcf21117a5
Bodomit <gbrown29@qub.ac.uk>
2021-04-26 20:35:50 +0100

    Multitask training mdel with tests.

---
+++ tests/models_test.py
@@ -0,0 +1,28 @@
+import os
+import unittest
+
+from PIL import Image
+from facenet_pytorch import MTCNN
+import torch
+
+from houshou.models import MultiTaskTrainingModel
+
+
+class MultiTaskTrainingModelTests(unittest.TestCase):
+    def __init__(self, methodName: str) -> None:
+        super().__init__(methodName=methodName)
+        current_dir = os.path.dirname(os.path.realpath(__file__))
+        image_path = os.path.join(current_dir, "resources", "0001_01.jpg")
+        test_image_pil = Image.open(image_path)
+        mtcnn = MTCNN(image_size=160, post_process=False)
+        self.test_image = mtcnn(test_image_pil).unsqueeze(0)
+
+    def test_forward_no_exception(self) -> None:
+        model = MultiTaskTrainingModel()
+        model.eval()
+        with torch.no_grad():
+            features, attribute = model(self.test_image)  # type: ignore
+            assert features is not None
+            assert features.shape == torch.Size([1, 512])
+            assert attribute is not None
+            assert attribute.shape == torch.Size([1, 2])
