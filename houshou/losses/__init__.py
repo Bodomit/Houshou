a5947e4289f911e33f86ce3e562cdf3e28df17a4
Bodomit <gbrown29@qub.ac.uk>
2021-04-28 18:34:27 +0100

    get_loss returning concrete variable.

--- houshou/losses/__init__.py
+++ houshou/losses/__init__.py
@@ -1,5 +1,5 @@
 from enum import Enum, auto
-
+from typing import Type
 import houshou.losses.semihard_triplet_miner as stm
 
 
@@ -7,8 +7,8 @@ class LOSS(Enum):
     SEMIHARD_CROSSENTROPY = auto()
 
 
-def get_loss(loss: LOSS):
+def get_loss(loss: LOSS, **kwargs) -> stm.SemiHardTripletMiner:
     if loss == LOSS.SEMIHARD_CROSSENTROPY:
-        return stm.SHTWithCategoricalCrossEntropy
+        return stm.SHTWithCategoricalCrossEntropy(**kwargs)
     else:
         raise ValueError()
