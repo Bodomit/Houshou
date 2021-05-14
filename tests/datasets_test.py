efe2ff40b23283d5e42d856bdc5fd51fbf10c70c
Bodomit <gbrown29@qub.ac.uk>
2021-05-14 23:56:28 +0100

    CelebA using DataModule.

--- tests/datasets_test.py
+++ tests/datasets_test.py
@@ -5,11 +5,9 @@ import pytest
 from PIL.Image import Image
 
 import torch
-from torch.utils.data import Dataset, DataLoader
-from torchvision import transforms
+from torch.utils.data import DataLoader
 
 from houshou.data import CelebA, VGGFace2
-from houshou.data import AttributeDataset
 from houshou.data.samplers import TripletBatchRandomSampler
 
 POSSIBLE_DATASET_ROOT_DIRS = ["/mnt/e/datasets", "~/datasets"]
@@ -27,61 +25,39 @@ def get_root_dir() -> str:
 class CelebADatasetTests(unittest.TestCase):
     def setUp(self):
         self.root = get_root_dir()
+        self.data_dir = os.path.join(self.root, "CelebA_MTCNN")
+        self.batch_size = 2
+        self.dataset_module = CelebA(
+            self.data_dir, self.batch_size, 1000, ["Male", "Bags_Under_Eyes"]
+        )
+        self.dataset_module.setup(None)
 
     def test_load_train(self):
-        dataset = CelebA(self.root, "train")
-        assert dataset is not None
-        assert len(dataset) == 162484
+        assert self.dataset_module.train is not None
+        assert len(self.dataset_module.train) == 162484
 
-        for image, target in dataset:
+        for image, target in self.dataset_module.train:
             assert image is not None
-            assert isinstance(image, Image)
+            assert isinstance(image, torch.Tensor)
             assert len(target) == 2
             assert isinstance(target[0], torch.Tensor)
             assert isinstance(target[1], torch.Tensor)
             assert isinstance(target[0].item(), int)
-            assert len(target[1]) > 1 and len(target[1].shape) == 1
+            assert len(target[1]) == 2 and len(target[1].shape) == 1
             break
 
     def test_load_test(self):
-        dataset = CelebA(self.root, "test")
-        assert dataset is not None
-        assert len(dataset) == 19944
+        assert self.dataset_module.test is not None
+        assert len(self.dataset_module.test) == 19944
 
-        for image, target in dataset:
+        for image, target in self.dataset_module.test:
             assert image is not None
-            assert isinstance(image, Image)
+            assert isinstance(image, torch.Tensor)
             assert len(target) == 2
             assert isinstance(target[0], torch.Tensor)
             assert isinstance(target[1], torch.Tensor)
             assert isinstance(target[0].item(), int)
-            assert len(target[1]) > 1 and len(target[1].shape) == 1
-            break
-
-
-@pytest.mark.local
-class AttributeDatasetTests(unittest.TestCase):
-    def setUp(self):
-        self.root = get_root_dir()
-
-    def test_celeba_train_male_attribute(self):
-        dataset: Dataset = CelebA(self.root, "train")
-        dataset = AttributeDataset(dataset, ["Male"])
-
-        for image, (identity, attributes) in dataset:
-            assert image is not None
-            assert identity.shape == torch.Size([])
-            assert attributes.shape == torch.Size([1])
-            break
-
-    def test_vggface2_test_male_attribute(self):
-        dataset: Dataset = VGGFace2(self.root, "test")
-        dataset = AttributeDataset(dataset, ["Male"])
-
-        for image, (identity, attributes) in dataset:
-            assert image is not None
-            assert identity.shape == torch.Size([])
-            assert attributes.shape == torch.Size([1])
+            assert len(target[1]) == 2 and len(target[1].shape) == 1
             break
 
 
@@ -90,27 +66,19 @@ class TripletFriendlyRandomSamplerTests(unittest.TestCase):
     def __init__(self, methodName: str) -> None:
         super().__init__(methodName=methodName)
         self.root = get_root_dir()
+        self.data_dir = os.path.join(self.root, "CelebA_MTCNN")
 
-        self.transform = transforms.Compose(
-            [
-                transforms.ToTensor(),  # Reads images scales to [0, 1]
-                transforms.Lambda(lambda x: x * 2 - 1),  # Change range to [-1, 1]
-                transforms.Resize((160, 160)),
-                transforms.RandomHorizontalFlip(p=0.5),
-            ]
-        )
-
-        dataset = CelebA(self.root, "train", transform=self.transform)
-        self.dataset = AttributeDataset(dataset, ["Male"])
+        self.dataset_module = CelebA(self.data_dir, 8, 1000, ["Male"])
+        self.identities = self.dataset_module.train.identities
 
     def test_sampler_batchs_have_valid_triplets(self):
         sampler = TripletBatchRandomSampler(
-            self.dataset, batch_size=32, drop_last=False, buffer_size=100
+            self.identities, batch_size=32, drop_last=False, buffer_size=100
         )
 
         i = 0
         for i, batch in enumerate(sampler):
-            identities = self.dataset.identity[batch]
+            identities = self.identities[batch]
             unique_identities, counts = identities.unique(return_counts=True)
             assert len(unique_identities) >= 2
             assert torch.any(counts >= 2)
@@ -119,12 +87,12 @@ class TripletFriendlyRandomSamplerTests(unittest.TestCase):
 
     def test_sampler_batchs_have_valid_triplets_drop_last(self):
         sampler = TripletBatchRandomSampler(
-            self.dataset, batch_size=32, drop_last=True, buffer_size=100
+            self.identities, batch_size=32, drop_last=True, buffer_size=100
         )
 
         i = 0
         for i, batch in enumerate(sampler):
-            identities = self.dataset.identity[batch]
+            identities = self.identities[batch]
             unique_identities, counts = identities.unique(return_counts=True)
             assert len(unique_identities) >= 2
             assert torch.any(counts >= 2)
@@ -133,10 +101,10 @@ class TripletFriendlyRandomSamplerTests(unittest.TestCase):
 
     def test_sampler_batchs_have_valid_triplets_full(self):
         sampler = TripletBatchRandomSampler(
-            self.dataset, batch_size=32, drop_last=True, buffer_size=100
+            self.identities, batch_size=32, drop_last=True, buffer_size=100
         )
 
-        for _, (yb, _) in DataLoader(self.dataset, batch_sampler=sampler):
+        for _, (yb, _) in DataLoader(self.dataset_module.train, batch_sampler=sampler):
             yb_unique, counts = yb.unique(return_counts=True)
             assert len(yb_unique) >= 2
             assert torch.any(counts >= 2)
