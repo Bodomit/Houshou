714704b341a897cc23394bb65f8fd98c397eb98b
Bodomit <gbrown29@qub.ac.uk>
2021-05-17 14:17:01 +0100

    Basic logging. Will do for now.

--- features_train.py
+++ features_train.py
@@ -1,11 +1,28 @@
-from pytorch_lightning.utilities.cli import LightningArgumentParser, LightningCLI
+from pytorch_lightning.utilities.cli import LightningCLI
+
+from pytorch_lightning.loggers import TestTubeLogger, CSVLogger
 
 from houshou.trainers import MultitaskTrainer
-from houshou.data import TripletsAttributeDataModule, DATASET, CelebA
+from houshou.data import TripletsAttributeDataModule
+
 
 cli = LightningCLI(
     MultitaskTrainer,
     TripletsAttributeDataModule,
     subclass_mode_data=True,
     parser_kwargs={"default_config_files": ["configs/default_config.yaml"]},
+    # trainer_defaults={
+    #     "logger": [
+    #         TestTubeLogger(
+    #             "/home/gbrown/results/houshou/tt",
+    #             name="semihard_crossentropy_0.5",
+    #             create_git_tag=True,
+    #             log_graph=True,
+    #         ),
+    #         CSVLogger(
+    #             "/home/gbrown/results/houshou/csv", name="semihard_crossentropy_0.5"
+    #         ),
+    #     ]
+    # },
+    seed_everything_default=42,
 )
