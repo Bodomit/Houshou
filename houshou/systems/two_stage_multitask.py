49a7e73680b78b1659636bedcebabb76dea18417
Glen Brown <gbrown29@qub.ac.uk>
2021-06-08 15:25:20 +0100

    Switching back to one optimizer. (#27)

--- houshou/systems/two_stage_multitask.py
+++ houshou/systems/two_stage_multitask.py
@@ -1,8 +1,9 @@
 from typing import Dict
+
 import torch
 import torch.nn.functional as F
-
 from houshou.metrics import CVThresholdingVerifier
+
 from .multitask_trainer import MultitaskTrainer
 
 
@@ -24,10 +25,32 @@ class TwoStageMultitaskTrainer(MultitaskTrainer):
             weight_attributes,
             **kwargs
         )
+        self.automatic_optimization = False
 
-    def training_step(self, batch, batch_idx, optimizer_idx):
+    def training_step(self, batch, batch_idx):
+        opt = self.optimizers()
+        assert isinstance(opt, torch.optim.Optimizer)
+        opt.zero_grad()
+
+        # Get the batch.
         xb, (yb, ab) = batch
         ab = ab.squeeze()
+
+        # Train attribute model only.
+        self.model.feature_model.requires_grad_(False)
+        self.model.attribute_model.requires_grad_(True)
+
+        embeddings, attribute_pred = self.model(xb)
+
+        attribute_loss = F.cross_entropy(attribute_pred, ab, self.attribute_weights)
+        self.log("loss/stage1", attribute_loss, on_step=True, on_epoch=True)
+        self.manual_backward(attribute_loss)
+        opt.step()
+        opt.zero_grad()
+
+        # Train on same batch but feature model only.
+        self.model.feature_model.requires_grad_(True)
+        self.model.attribute_model.requires_grad_(False)
 
         embeddings, attribute_pred = self.model(xb)
 
@@ -35,31 +58,20 @@ class TwoStageMultitaskTrainer(MultitaskTrainer):
         metrics = self.training_step_attribute_metrics(ab, attribute_pred)
         self.log_dict(metrics)
 
-        # Train attribute model only.
-        if optimizer_idx == 0:
-            attribute_loss = F.cross_entropy(attribute_pred, ab, self.attribute_weights)
-            self.log("loss/stage1", attribute_loss, on_step=True, on_epoch=True)
-            return attribute_loss
-
-        # Train on feature model only.
-        if optimizer_idx == 1:
-            assert isinstance(embeddings, torch.Tensor)
-            total_loss, sub_losses = self.get_totalloss_with_sublosses(
-                self.loss, yb, ab, embeddings, attribute_pred, prefix="loss/stage2/"
-            )
-            self.log("loss/stage2/total", total_loss, on_step=True, on_epoch=True)
-            self.log_dict(sub_losses, on_step=True, on_epoch=True)
-
-            return total_loss
+        # Backprop the multi-task loss.
+        assert isinstance(embeddings, torch.Tensor)
+        total_loss, sub_losses = self.get_totalloss_with_sublosses(
+            self.loss, yb, ab, embeddings, attribute_pred, prefix="loss/stage2/"
+        )
+        self.log("loss/stage2/total", total_loss, on_step=True, on_epoch=True)
+        self.log_dict(sub_losses, on_step=True, on_epoch=True)
+        self.manual_backward(total_loss)
+        opt.step()
+        opt.zero_grad()
 
     def configure_optimizers(self):
-        optimizers = [
+        return (
             torch.optim.Adam(
                 self.model.attribute_model.parameters(), lr=self.learning_rate
             ),
-            torch.optim.Adam(
-                self.model.feature_model.parameters(), lr=self.learning_rate
-            ),
-        ]
-
-        return optimizers
+        )
