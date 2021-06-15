051da03b083c2287f3adf811142752c3c9694336
Bodomit <gbrown29@qub.ac.uk>
2021-06-15 18:09:01 +0100

    Cleanup unused dataset stuff.

--- houshou/data/__init__.py
+++ houshou/data/__init__.py
@@ -7,23 +7,3 @@ from .base import TripletsAttributeDataModule
 from .celeba import CelebA
 from .market_1501 import Market1501
 from .vggface2 import VGGFace2
-
-
-class DATASET(Enum):
-    VGGFACE2 = auto()
-    CELEBA = auto()
-    LFW = auto()
-
-    def __str__(self) -> str:
-        return self.name
-
-
-def get_dataset_module(dataset: DATASET, **kwargs) -> Union[CelebA, VGGFace2]:
-    if dataset == DATASET.CELEBA:
-        dataset_module = CelebA(**kwargs)
-    elif dataset == DATASET.VGGFACE2:
-        dataset_module = VGGFace2(**kwargs)
-    else:
-        raise ValueError()
-
-    return dataset_module
