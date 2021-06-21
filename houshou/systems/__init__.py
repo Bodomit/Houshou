1719f7499bfcee3a617acae751ab4f96624bd05e
Bodomit <gbrown29@qub.ac.uk>
2021-06-21 17:52:54 +0100

    Initial Alvi2019

--- houshou/systems/__init__.py
+++ houshou/systems/__init__.py
@@ -1,3 +1,4 @@
 from .multitask_trainer import MultitaskTrainer
 from .two_stage_multitask import TwoStageMultitaskTrainer
 from .attribute_extraction import AttributeExtractionTask
+from .alvi2019 import Alvi2019
