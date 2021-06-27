97a7a5002b3ce3a98a010299a26271ae2126f045
Bodomit <gbrown29@qub.ac.uk>
2021-06-27 17:36:59 +0100

    Add RAP2 to __init__.py for datasets

--- houshou/data/__init__.py
+++ houshou/data/__init__.py
@@ -6,4 +6,5 @@ from torch.utils.data import Dataset
 from .base import TripletsAttributeDataModule
 from .celeba import CelebA
 from .market_1501 import Market1501
+from .rap2 import RAP2
 from .vggface2 import VGGFace2
