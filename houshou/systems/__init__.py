39e5ccac31132ae240cc2fbdcf65574e9b57d1f5
Bodomit <gbrown29@qub.ac.uk>
2021-07-02 21:57:48 +0100

    Initial Attacker AEM experiment.

--- houshou/systems/__init__.py
+++ houshou/systems/__init__.py
@@ -1,4 +1,4 @@
+from .alvi2019 import Alvi2019
+from .attribute_extraction import AttributeExtractionTask
 from .multitask_trainer import MultitaskTrainer
 from .two_stage_multitask import TwoStageMultitaskTrainer
-from .attribute_extraction import AttributeExtractionTask
-from .alvi2019 import Alvi2019
