18261c94dc51805dd59ccca49e9bda1cdf7518ff
bodomit <gbrown29@qub.ac.uk>
2021-09-27 21:24:19 +0100

    Overwirte config file on reschedule.

--- features_train.py
+++ features_train.py
@@ -16,4 +16,5 @@ cli = CustomLightningCLI(
     subclass_mode_data=True,
     parser_kwargs={"default_config_files": ["configs/default_config.yaml"]},
     seed_everything_default=42,
+    save_config_overwrite=True
 )
